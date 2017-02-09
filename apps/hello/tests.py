a447b507fad675caa465706f5d24fcadd919f1a6
volartem <artem.volshynskyi@gmail.com>
2017-02-09 16:27:47 +0200

    ticket:1 Create home.html

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
-
 
 class SomeTests(TestCase):
     def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+        """put docstrings in your tests"""
+        assert(2 + 2 == 4)
