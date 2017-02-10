4b856868ab64753185942e1343fb3617610ea4ff
volartem <artem.volshynskyi@gmail.com>
2017-02-10 15:39:41 +0200

    ticket:1 created test for the model

---
+++ apps/hello/tests/tests_models.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from apps.hello.models import Contact
+from django.utils.encoding import smart_unicode
+from datetime import datetime, date
+
+
+class ContactModelTest(TestCase):
+    """
+    Test Contact model
+    """
+
+    def setUp(self):
+        """
+        Setup two objects to tests
+        """
+        Contact.objects.create(name="testName",
+                               surname='testSurname',
+                               date_birth="2002-02-02",
+                               bio='test bio info\n test bio info2',
+                               email='test@email.com',
+                               jabber='test@jabber.com',
+                               skype='test_skype',
+                               other_contacts='test contact info',
+                               )
+        Contact.objects.create(name="testName2",
+                               surname='testSurname2',
+                               date_birth="2002-02-03",
+                               bio='test bio info3\n test bio info4',
+                               email='test@email.com2',
+                               jabber='test@jabber.com2',
+                               skype='test_skype2',
+                               other_contacts='test contact info2',)
+
+    def test_unicode(self):
+        """
+        Test unicode returns correct
+        """
+
+        contact = Contact(name=u'Джуниор', surname=u'Джанго')
+        self.assertEqual(smart_unicode(contact), u'Джуниор Джанго')
+
+    def test_model_instance_first(self):
+        """
+        Test first instance that would be in template
+        """
+        contact = Contact.objects.first()
+        self.assertIsInstance(contact.created, datetime)
+        self.assertIsInstance(contact.name, unicode)
+        self.assertIsInstance(contact.date_birth, date)
+        self.assertEqual(contact.name, u'testName')
+        self.assertEqual(contact.surname, u'testSurname')
+        self.assertEqual(contact.date_birth, date(2002, 02, 02))
+        self.assertEqual(contact.email, u'test@email.com')
+        self.assertEqual(contact.bio, u'test bio info\n test bio info2')
+
+    def test_model_instance_last(self):
+        """
+        Test last instance
+        """
+        contact = Contact.objects.last()
+        self.assertIsInstance(contact.created, datetime)
+        self.assertIsInstance(contact.name, unicode)
+        self.assertIsInstance(contact.date_birth, date)
+        self.assertEqual(contact.date_birth, date(2002, 02, 03))
+        self.assertEqual(contact.name, u'testName2')
+        self.assertEqual(contact.surname, u'testSurname2')
+        self.assertEqual(contact.email, u'test@email.com2')
+        self.assertEqual(contact.skype, u'test_skype2')
+        self.assertEqual(contact.bio, u'test bio info3\n test bio info4')
