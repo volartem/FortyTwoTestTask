35f84b1437f485cd32fe4af0331ea70d99542339
volartem <artem.volshynskyi@gmail.com>
2017-02-11 12:56:08 +0200

    ticket:1 code admin.py

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from apps.hello import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -8,3 +9,5 @@ urlpatterns = patterns(
     url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
