5bd2b3df9d54278876049352763d2c8d37b580e4
volartem <artem.volshynskyi@gmail.com>
2017-02-13 19:24:24 +0200

    ticket:1 created test for views home with two objects and for model Contact

--- apps/hello/tests/tests_models.py
+++ apps/hello/tests/tests_models.py
@@ -31,7 +31,7 @@ class ContactModelTest(TestCase):
         contact = Contact(name=u'Джуниор', surname=u'Джанго')
         self.assertEqual(smart_unicode(contact), u'Джуниор Джанго')
 
-    def test_model_instance_first(self):
+    def test_contact_instance_first(self):
         """
         Test first instance that would be in template
         """
@@ -46,3 +46,27 @@ class ContactModelTest(TestCase):
         self.assertEqual(contact.email, u'test@email.com2')
         self.assertEqual(contact.skype, u'test_skype2')
         self.assertEqual(contact.bio, u'test bio info3\n test bio info4')
+
+    def test_contact_instance_last(self):
+        """
+        Test second instance that created
+        """
+        Contact.objects.create(
+            name="testName22",
+            surname='testSurname22',
+            date_birth="2002-02-04",
+            bio='test bio info5\n test bio info6',
+            email='test@email.com22',
+            jabber='test@jabber.com22',
+            skype='test_skype22',
+            other_contacts='test contact info22',)
+        contact = Contact.objects.last()
+        self.assertIsInstance(contact.created, datetime)
+        self.assertIsInstance(contact.name, unicode)
+        self.assertIsInstance(contact.date_birth, date)
+        self.assertEqual(contact.date_birth, date(2002, 02, 04))
+        self.assertEqual(contact.name, u'testName22')
+        self.assertEqual(contact.surname, u'testSurname22')
+        self.assertEqual(contact.email, u'test@email.com22')
+        self.assertEqual(contact.skype, u'test_skype22')
+        self.assertEqual(contact.bio, u'test bio info5\n test bio info6')
--- apps/hello/tests/tests_views.py
+++ apps/hello/tests/tests_views.py
@@ -32,26 +32,34 @@ class HomeViewTest(TestCase):
 
     def test_home_view_without_data(self):
         """
-        Table of DB is empty, must be presented 'No contact info'
+        Test table of DB is empty, must be presented 'No contact info'
         """
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertIn('42 Coffee Cups Test Assignment', response.content)
         self.assertIn('No contact info', response.content)
-        self.assertNotIn('myName', response.content)
-        self.assertNotIn('mySurname', response.content)
+        self.assertNotIn('testName', response.content)
+        self.assertNotIn('testSurname', response.content)
+        self.assertEqual(response.context['info'], None)
 
-    def test_home_template_context(self):
+    def test_home_view_template_base(self):
         """
-        Check home status code and context
+        Test home view with base elements
         """
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
+        self.assertInHTML('<link rel="stylesheet" href="/static/css/bootstrap/'
+                          'bootstrap.min.css">', response.content)
+        self.assertInHTML('<link rel="stylesheet" href="/static/css/style.css" '
+                          'media="screen, projection">', response.content)
+        self.assertInHTML('<li class="active"><a href="/">Home</a></li>', response.content)
         self.assertTrue('info' in response.context)
+        self.assertContains(response, '42 Coffee Cups Test Assignment')
+
 
     def test_home_template_content(self):
         """
-        Check info to the template
+        Check info to the template with one object
         """
         self.create_obj()
         response = self.client.get(self.url)
@@ -64,9 +72,14 @@ class HomeViewTest(TestCase):
         self.assertEqual(data.email, 'email@email.com')
         self.assertEquals(data.skype, 'skype')
         self.assertEqual(data.jabber, 'jabber@co')
+        self.assertContains(response, 'Name')
+        self.assertContains(response, 'Last name')
+        self.assertContains(response, 'email')
 
     def test_home_view_cyrillic(self):
-        """Test for views.home in case object data is cyrillic"""
+        """
+        Test for views.home in case object data is cyrillic
+        """
         Contact.objects.create(
             name='Артем',
             surname='Александрович',
@@ -85,3 +98,40 @@ class HomeViewTest(TestCase):
         self.assertIn('Date of birth', response.content)
         self.assertIn('Био', response.content)
         self.assertIn('Другие контакты', response.content)
+        self.assertContains(response, 'Name')
+        self.assertContains(response, 'Last name')
+        self.assertContains(response, 'Email')
+
+    def test_home_view_two_object(self):
+        """
+        Test home view, table Contact has 2 objects,
+        """
+        # first object
+        self.create_obj()
+
+        # second object
+        Contact.objects.create(
+            name='testName',
+            surname='testSurname',
+            date_birth='1986-11-07',
+            bio='Django Python developer Dublh 3 '
+                '\r\nJunior Django Python developer',
+            email='email@email.com',
+            jabber='jabber@co',
+            skype='skype',
+            other_contacts='test contact info')
+
+        response = self.client.get(self.url)
+        data = response.context['info']
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(data, None)
+        self.assertEqual(Contact.objects.first(), data)
+        self.assertEqual(Contact.objects.count(), 2)
+        self.assertEqual(data.name, 'testName')
+        self.assertEqual(data.surname, 'testSurname')
+        self.assertEqual(data.date_birth.strftime('%d-%m-%Y'), '07-11-1986')
+        self.assertEqual(data.bio, 'Django Python developer Dublh 3 '
+                                   '\r\nJunior Django Python developer')
+        self.assertEqual(data.email, 'email@email.com')
+        self.assertEquals(data.skype, 'skype')
+        self.assertEqual(data.jabber, 'jabber@co')
